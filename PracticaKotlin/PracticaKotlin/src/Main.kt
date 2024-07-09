8e2f5be5b8cff27247934f41a8d9ac31a8738c2b
Ivan93LA <ivan.inln17@gmail.com>
2024-07-09 12:28:08 +0200

    Primera versíon

---
+++ PracticaKotlin/PracticaKotlin/src/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    println("Hola Kotlin")
+
+    // variables
+
+    var myString = "Esto es una cadena de texto"
+    myString = "Aqui cambio el valor de la cadena de texto"
+    myString = "6"
+    println(myString)
+
+    var myString2 = "Esto es otra cadena de texto"
+    println(myString2)
+
+    var myInt = 7
+    myInt = myInt + 4
+    println(myInt - 1) // Mantiene siempre el myInt
+
+    var myInt2: Int = 5
+    println(myInt2)
+
+    var myDouble = 6.5
+    println(myDouble)
+    // Los double siempre son decimales
+    myDouble = 6.0
+    println(myDouble)
+
+    var myDouble2: Double = 6.5
+    var myFloat = 6.5f
+
+    var myBool = true
+    myBool = true
+    println(myBool)
+}
